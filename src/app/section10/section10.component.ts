0e4d9bcfa623e19be99e8e2390a47b51471c9c68
chad <sanachix@gmail.com>
2018-08-14 15:41:52 -0400

    Includes the signature line

--- src/app/section10/section10.component.ts
+++ src/app/section10/section10.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 import { Section10Service } from '@app/section10/section10.service';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad'
 
 @Component({
   selector: 'app-section10',
   templateUrl: './section10.component.html',
-  styleUrls: ['./section10.component.scss']
+  styleUrls: ['./section10.component.scss'],
+  
 })
 export class Section10Component implements OnInit {
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
 
   states:string[];
 
+  
+  private signaturePadOptions: Object = { // passed through to szimek/signature_pad constructor
+    'minWidth': 0.5,
+    'maxWidth': 1,
+    'canvasWidth': 500,
+    'canvasHeight': 100,
+    'dotSize': 0.5,
+    'minDistance': 1
+  };
+ 
+
   constructor(service:Section10Service) { 
     this.states=service.getStates();
+  }
+
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
+
+  drawComplete() {
+    // will be notified of szimek/signature_pad's onEnd event
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('begin drawing');
   }
 
 
--- src/app/section10/section10.module.ts
+++ src/app/section10/section10.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Section10Component } from '@app/section10/section10.component';
 import { BrowserModule } from '../../../node_modules/@angular/platform-browser';
 import { DxFormModule, DxCheckBoxModule, DxButtonModule, DxAutocompleteModule, DxNumberBoxModule, DxListModule} from '../../../node_modules/devextreme-angular';
+import { SignaturePadModule } from 'angular2-signaturepad'
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { DxFormModule, DxCheckBoxModule, DxButtonModule, DxAutocompleteModule, D
     DxAutocompleteModule,
     DxNumberBoxModule,
     DxListModule,
+    SignaturePadModule,
 
   ],
   declarations: [Section10Component],
